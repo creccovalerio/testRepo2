f7aed2a34a55bc12c3d44553de7cca829e01aad8
creccovalerio <valerio.crecco@students.uniroma2.eu>
2022-03-28 20:44:49 +0200

    fourth commint

---
+++ src/test/java/isw2examples/test3/ExampleTest.java
@@ -0,0 +1,11 @@
+package isw2examples.test3;
+
+import junit.framework.TestCase;
+
+public class ExampleTest extends TestCase {
+	
+	public void testExample()
+    {
+        assertTrue( true );
+    }
+}
